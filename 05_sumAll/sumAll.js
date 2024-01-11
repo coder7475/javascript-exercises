3615f51376e3e1bdae2f15b77a958d476c536d31
coder7475 <robiulhossain7475@gmail.com>
2024-01-11 23:20:53 +0600

    feat: solved exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (a < 0 || b < 0 || typeof(a) != "number" || typeof(b) != "number")
+    return "ERROR";
 
+  let sum = 0;
+  let low = 0;
+  let high = 0;
+  if (a < b){
+    low = a;
+    high = b;
+  } else {
+    low = b;
+    high = a;
+  }
+
+  for (let index = low; index <= high; index++)
+    sum += index;      
+
+  return sum;
 };
 
 // Do not edit below this line
