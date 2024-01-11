7290b4f47e0b3dc3ee477ec731a85e68bda43298
coder7475 <robiulhossain7475@gmail.com>
2024-01-11 22:58:56 +0600

    feat: solved exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  const length = word.length;
+  let newString = "";
 
+  for (let x = length - 1; x >= 0; x--) {
+    newString += word[x];
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
