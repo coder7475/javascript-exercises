3554fd7278f2c3e06c2c4d425586f966d7462ca8
coder7475 <robiulhossain7475@gmail.com>
2024-01-11 23:09:48 +0600

    feat: solved exercise 4 with all test case passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, ...rest) {
+  const ans = [];
+  const l = givenArray.length;
 
+  for (let i = 0; i < l; i++) {
+    if (!rest.includes(givenArray[i])) {
+      ans.push(givenArray[i])
+    }
+  }
+
+  return ans;
 };
 
 // Do not edit below this line
